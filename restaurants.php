d44adba9fecad754156f166e6e19cca79a5c82cc
ChristianJadot <christian.jadot@gmail.com>
2021-03-31 09:23:52 +0200

    upload restaurants php

---
+++ restaurants.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/5c3cdba868.js" crossorigin="anonymous"></script>
+        <script src="becool.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>BeCool Cooking - Restaurants</title>
+    </head>
+<body>
+    <div class="container">
+        <!-- logo & navigation-->
+        <?php include("logoNav.php"); ?>
+
+        <!-- content -->
+        <main>
+            <section class="row">
+                <section class="col-12">
+                    <img src="images/restaurants-BeCool-Cooking.jpg" alt="Les enseignes BeCool Cooking">
+                </section>
+            </section>
+            <!-- Bruxelles -->
+            <section class="row p-2 m-1 my-5 border border-dashed border-info">
+                <section class="col-12">
+                    <h4>Bruxelles <a href="contact.html" class="float-right"><button type="button" class="btn btn-info btn-sm ml-3">Réservez votre table</button></a></h4>
+                </section>
+                <section class="col-12" >
+                    <section class="row">
+                        <section class="col-md-5">
+                            <h5>Adresse</h5>
+                            <address>
+                                <p>Rue à la Folle Cuisine 9<br>
+                                    1000 Bruxelles<br>
+                                    <a href="#" class="email"><i class="fas fa-envelope"></i> bruxelles@becool.com</a><br>
+                                    <a href="#" class="tel"><i class="fas fa-phone-alt"></i> +32 2 999 99 99</a>
+                                </p>
+                            </address>
+                            <section class="horaire">
+                                <h5>Horaire</h5>
+                                <section class="row">
+                                    <section class="col-4">
+                                        <p class="my-0">Lu - Ma</p>
+                                        <p class="my-0">Me - Je - Ve</p>
+                                        <p class="my-0">Sa - Di</p>
+                                   </section>
+                                    <section class="col text-left">
+                                        <p class="my-0">Fermé</p>
+                                        <p class="my-0">12h à 16h - 18h à 22h</p>
+                                        <p class="my-0">18h à 23h</p>
+                                    </section>
+                                </section>
+                        </section>
+                        </section>
+                        <section class="col-xs-12 col-md-7 py-2">
+                            <h5>Comment nous rejoindre ?</h5>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10075.78023153411!2d4.344735699659897!3d50.85070167412318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c381263941e5%3A0xb4a4fbe83b6d9479!2sRue%20du%20Foss%C3%A9%20aux%20Loups%2018%2C%201000%20Bruxelles!5e0!3m2!1sfr!2sbe!4v1612359246306!5m2!1sfr!2sbe" width="100%" height="auto" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </section>
+                    </section>
+                </section>
+            </section>
+            <!-- Gent -->
+            <section class="row p-2 m-1 my-5 border border-dashed border-info">
+                <section class="col-12">
+                    <h4>Gent <a href="contact.html" class="float-right"><button type="button" class="btn btn-info btn-sm ml-3">Réservez votre table</button></a></h4>
+                </section>
+                <section class="col-12" >
+                    <section class="row">
+                        <section class="col-md-5">
+                            <h5>Adresse</h5>
+                            <address>
+                                <p>Sint-Baafsplein 16<br>
+                                    9000 Gent<br>
+                                    <a href="#" class="email"><i class="fas fa-envelope"></i> gent@becool.com</a><br>
+                                    <a href="#" class="tel"><i class="fas fa-phone-alt"></i> +32 9 999 99 99</a>
+                                </p>
+                            </address>
+                            <section class="horaire">
+                                <h5>Horaire</h5>
+                                <section class="row">
+                                    <section class="col-4">
+                                        <p class="my-0">Lu - Ma</p>
+                                        <p class="my-0">Me - Je - Ve</p>
+                                        <p class="my-0">Sa - Di</p>
+                                   </section>
+                                    <section class="col text-left">
+                                        <p class="my-0">Fermé</p>
+                                        <p class="my-0">12h à 16h - 18h à 22h</p>
+                                        <p class="my-0">18h à 23h</p>
+                                    </section>
+                                </section>
+                        </section>
+                        </section>
+                        <section class="col-xs-12 col-md-7 py-2">
+                            <h5>Comment nous rejoindre ?</h5>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1253.9867340812532!2d3.7249568860803484!3d51.05357769763772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c371461579f6ab%3A0x1191eff56a13be98!2sSint-Baafsplein%2016%2C%209000%20Gent!5e0!3m2!1sfr!2sbe!4v1612363903435!5m2!1sfr!2sbe" width="100%" height="auto" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </section>
+                    </section>
+                </section>
+            </section>
+            <!-- Liège -->
+            <section class="row p-2 m-1 my-5 border border-dashed border-info">
+                <section class="col-12">
+                    <h4>Liège <a href="contact.html" class="float-right"><button type="button" class="btn btn-info btn-sm ml-3">Réservez votre table</button></a></h4>
+                </section>
+                <section class="col-12" >
+                    <section class="row">
+                        <section class="col-md-5">
+                            <h5>Adresse</h5>
+                            <address>
+                                <p>Rue Saint-Gilles 4<br>
+                                    4000 Liège<br>
+                                    <a href="#" class="email"><i class="fas fa-envelope"></i> liege@becool.com</a><br>
+                                    <a href="#" class="tel"><i class="fas fa-phone-alt"></i> +32 4 999 99 99</a>
+                                </p>
+                            </address>
+                            <section class="horaire">
+                                <h5>Horaire</h5>
+                                <section class="row">
+                                    <section class="col-4">
+                                        <p class="my-0">Lu - Ma</p>
+                                        <p class="my-0">Me - Je - Ve</p>
+                                        <p class="my-0">Sa - Di</p>
+                                   </section>
+                                    <section class="col text-left">
+                                        <p class="my-0">Fermé</p>
+                                        <p class="my-0">12h à 16h - 18h à 22h</p>
+                                        <p class="my-0">18h à 23h</p>
+                                    </section>
+                                </section>
+                        </section>
+                        </section>
+                        <section class="col-xs-12 col-md-7 py-2">
+                            <h5>Comment nous rejoindre ?</h5>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.5227364648167!2d5.556122615733436!3d50.63598227950138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0fa18bc3b6d0f%3A0xa4092070e84704c3!2sRue%20Saint-Gilles%2C%204000%20Li%C3%A8ge!5e0!3m2!1sfr!2sbe!4v1612370698151!5m2!1sfr!2sbe" width="100%" height="auto" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </section>
+                    </section>
+                </section>
+            </section>
+        </main>
+
+        <!-- footer -->
+        <?php include("footer.php"); ?>
+        
+    </section>
+
+    <!-- jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
