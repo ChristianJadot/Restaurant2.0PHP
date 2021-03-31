c7ae8a992b278ee580822780c8416f80010a695c
ChristianJadot <christian.jadot@gmail.com>
2021-03-31 09:22:41 +0200

    upload gallery bxl php

---
+++ gallery-bruxelles.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/5c3cdba868.js" crossorigin="anonymous"></script>
+        <script src="becool.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>BeCool Cooking - Galerie</title>
+    </head>
+<body>
+    <section class="container">
+        <!-- logo & navigation-->
+        <?php include("logoNav.php"); ?>
+
+        <!-- content -->
+        <main>
+            <section class="container">
+                <section class="row">
+                    <section class="col-md-12">
+                        <h3 class="text-center">Galerie Bruxelles</h3>
+                        <section id="grid" class="row">
+                            <section class="col-sm-3 margin30">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-01.jpg" class="img-responsive thumbnail" data-gallery="gallery1" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-01.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section>            
+                            </section>
+                            <section class="col-sm-3 margin30">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-02.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-02.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section>            
+                            </section>
+                            <section class="col-sm-3 margin30 ">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-03.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-03.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section>             
+                            </section>
+                            <section class="col-sm-3 margin30">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-04.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-04.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section>           
+                            </section>
+                            <section class="col-sm-3 margin30 ">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-05.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-05.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section>           
+                            </section>
+                            <section class="col-sm-3 margin30">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-06.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-06.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section>            
+                            </section>
+                            <section class="col-sm-3 margin30">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-01.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-01.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section> 
+                            </section>
+                            <section class="col-sm-3 margin30">
+                                <section class="item-img-wrap ">
+                                    <img src="carousel/carousel-02.jpg" class="img-responsive" alt="BeCool Cooking">
+                                    <section class="item-img-overlay">
+                                        <a href="carousel/carousel-02.jpg" class="show-image" target="_blank">
+                                            <span></span>
+                                        </a>
+                                    </section>
+                                </section> 
+                            </section>                                                            
+                        </section>
+                    </section>
+                </section>
+                <section class="row gallery-bottom">
+                    <section class="col-sm-12">
+                        <ul class="pagination justify-content-center">
+                            <li><a href="#" aria-label="Previous"><span aria-hidden="false">«</span></a></li>
+                            <li class="active"><a href="#">1</a></li>
+                            <li><a href="gallery-gent.php">2</a></li>
+                            <li><a href="gallery-liege.php">3</a></li>
+                            <li><a href="gallery-gent.php" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                        </ul>
+                    </section>
+                </section>
+            </section>
+        </main>
+
+        <!-- footer -->
+        <?php include("footer.php"); ?>
+        
+    </section>
+
+    <!-- jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
